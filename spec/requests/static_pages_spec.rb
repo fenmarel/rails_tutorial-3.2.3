e727e1216262e936473dbc67af5bced2bb0f2f40
fenmarel <jonny.p.tatoes@gmail.com>
2013-12-02 12:47:54 -0800

    set up cleaner routes for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Static pages" do
 	describe "Home page" do
 		it "should have the content 'Sample App'" do 
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_content('Sample App')
 		end 
 
 		it "should have the correct title" do
-			visit '/static_pages/home'
+			visit root_path
 			page.should have_selector('title', text: "Rails Tutorial")
 			page.should_not have_selector('title', text: "| Home")
 		end
@@ -16,25 +16,37 @@ describe "Static pages" do
 
 	describe "Help page" do
 		it "should have the content 'Help'" do 
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_content('Help')
 		end
 		
 		it "should have the correct title" do
-			visit '/static_pages/help'
+			visit help_path
 			page.should have_selector('title', text: "Rails Tutorial | Help")
 		end
 	end
 
 	describe "About page" do 
 		it "should have content 'About'" do 
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_content('About')
 		end
 		
 		it "should have the correct title" do
-			visit '/static_pages/about'
+			visit about_path
 			page.should have_selector('title', text: "Rails Tutorial | About")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the content 'Contact'" do 
+			visit contact_path
+			page.should have_content('Contact')
+		end
+		
+		it "should have the correct title" do
+			visit contact_path
+			page.should have_selector('title', text: "Rails Tutorial | Contact")
 		end
 	end
 end
